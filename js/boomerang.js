37d6040c102e1a266d16bea06b513e9b3977b665
facevedo-meli <federico.acevedo@mercadolibre.com>
2018-04-02 23:20:21 -0300

    viene bien

--- js/boomerang.js
+++ js/boomerang.js
@@ -18,8 +18,8 @@
   
       // Disables the default Google Maps UI components
       disableDefaultUI: true,
-      scrollwheel: true,
-      draggable: true,
+      scrollwheel: false,
+      draggable: false,
   
       // How you would like to style the map.
       // This is where you would paste any style found on Snazzy Maps.
